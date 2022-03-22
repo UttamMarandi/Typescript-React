db6504d3c01d4ff86ccda80cdb6f154741066655
Uttam Marandi <uttammarandi007@gmail.com>
2022-03-22 23:11:44 +0530

    typescript basices - object

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Greet from "./Components/Greet";
+import Person from "./Components/Person";
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Uttam" messageCount={12} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
